35118eb74f72df09198b5997138901160d12333c
Sungho95 <tjdgh1785@gmail.com>
2022-07-11 18:10:11 +0900

    2022-07-11

--- src/main/kotlin/chap02/section2/minmax.kt
+++ src/main/kotlin/chap02/section2/minmax.kt
@@ -6,6 +6,13 @@ fun main() {
     println("Int: ${Int.MIN_VALUE}~${Int.MAX_VALUE}")
     println("Short: ${Short.MIN_VALUE}~${Short.MAX_VALUE}")
     println("Long: ${Long.MIN_VALUE}~${Long.MAX_VALUE}")
+
+    println("UByte: ${UByte.MIN_VALUE}~${UByte.MAX_VALUE}")
+    println("UInt: ${UInt.MIN_VALUE}~${UInt.MAX_VALUE}")
+    println("UShort: ${UShort.MIN_VALUE}~${UShort.MAX_VALUE}")
+    println("ULong: ${ULong.MIN_VALUE}~${ULong.MAX_VALUE}")
+
     println("Float: ${Float.MIN_VALUE}~${Float.MAX_VALUE}")
     println("Double: ${Double.MIN_VALUE}~${Double.MAX_VALUE}")
+
 }
