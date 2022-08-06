edc32eca7c0e08c023275b1b01085322f5ed2e76
Sungho95 <tjdgh1785@gmail.com>
2022-08-06 23:43:02 +0900

    2022-08-06

--- src/main/kotlin/chap06/section7/WithTest.kt
+++ src/main/kotlin/chap06/section7/WithTest.kt
@@ -8,7 +8,8 @@ fun main() {
     val result = with (user) { //this 생략 가능
         skills = "Kotlin"
         email = "kildong@example.com"
-        "Kotlin Master"
+        // 기본적으로 Unit 반환, 필요하면 다음과 같이 마지막 표현식 반환할 수 있음.
+        "Kotlin Master" // 마지막 표현식 반환
     }
     println(user)
     println("result: $result")
